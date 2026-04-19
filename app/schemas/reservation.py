68988c3b9ffe800ab688b1ef70f71a734c77bf5f
kenmoh <kenneth.aremoh@gmail.com>
2026-04-19 16:33:01 +0100

    remove table_id from schema

--- app/schemas/reservation.py
+++ app/schemas/reservation.py
@@ -3,13 +3,15 @@ from pydantic import BaseModel
 
 class CreateBooking(BaseModel):
     vendor_id: str
-    table_id: str
+    customer_id: str
     reservation_time: str
-    end_time: str
+    reservation_date: str
+    serving_period: str
     party_size: int
     number_of_children: int
-    number_of_adult: int
+    number_of_adults: int
     deposit_required: int
     deposit_paid: int
     notes: str | None
     business_name: str
+   
